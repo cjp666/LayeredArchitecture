18facb8e7b3a3f2c4b03e5cc8b76d03a0ff10dc6
Chris <chris.j.podmore@gmail.com>
2016-06-30 21:27:05 +0100

    Started building the Windows Service that hosts the WebAPI using OWIN

---
+++ WindowsService/IPAddress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WindowsService
+{
+    public static class IPAddress
+    {
+        public static string Get()
+        {
+            var a = Dns.GetHostAddresses(Dns.GetHostName()).AsQueryable();
+            var result = String.Empty;
+
+            foreach (var addr in a)
+            {
+                if (addr.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    result = addr.ToString();
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ WindowsService/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var asConsole = String.Compare(ConfigurationManager.AppSettings["asConsole"], "true", StringComparison.CurrentCultureIgnoreCase) == 0;
+
+            if (asConsole)
+            {
+                AsConsoleApplication();
+            }
+            else
+            {
+                //var servicesToRun = new ServiceBase[]
+                //{
+                //    new WTPAPIService()
+                //};
+                //ServiceBase.Run(servicesToRun);
+            }
+
+        }
+
+        private static void AsConsoleApplication()
+        {
+            var ipAddress = ConfigurationManager.AppSettings["useIP"];
+            if (String.IsNullOrWhiteSpace(ipAddress))
+            {
+                ipAddress = IPAddress.Get();
+            }
+
+            var port = ConfigurationManager.AppSettings["port"];
+            if (String.IsNullOrWhiteSpace(port))
+            {
+                port = "9021";
+            }
+
+            var useHTTPS = String.Compare(ConfigurationManager.AppSettings["useHTTPS"], "true", StringComparison.CurrentCultureIgnoreCase) == 0;
+            var useLocalhost = String.Compare(ConfigurationManager.AppSettings["useLocalhost"], "true", StringComparison.CurrentCultureIgnoreCase) == 0;
+
+            if (useLocalhost)
+            {
+                ipAddress = "localhost";
+            }
+
+            var baseAddress = String.Format("http{0}://{1}:{2}/", useHTTPS ? "s" : String.Empty, ipAddress, port);
+        }
+    }
+}
